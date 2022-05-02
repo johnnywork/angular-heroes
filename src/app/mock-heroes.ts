ece09f730893fb83d8fde52f4f9a910b202d7b70
johnny <lazaridis.work@gmail.com>
2022-05-02 23:25:43 +0200

    v1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id:11, name:"Iron-man"},
+    {id:12, name:"Bat-man"},
+    {id:13, name:"Spinter-man"},
+    {id:14, name:"Hulk"},
+    {id:15, name:"Captain America"},
+    {id:16, name:"Thor"},
+    {id:17, name:"Black widow"},
+    {id:18, name:"Black Panther"},
+    {id:19, name:"Doctor strange"},
+    {id:20, name:"Ant-man"}
+]
