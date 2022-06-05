052f80ef981727de8408aa3d2aa323d035e1b44b
Lazaridis John <lazaridis.work@gmail.com>
2022-06-05 18:07:16 +0200

    E3E

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { Observable,  of } from 'rxjs';
+import { Observable,  of , map} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'http://localhost:8080/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {
+    }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    let heroes = this.http.get<Hero[]>(this.heroesUrl+'/all');
+    this.log('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    let hero = this.http.get<Hero>(this.heroesUrl+'/'+id);
+    this.log(`HeroService: fetched hero id=${id}`);
+    return hero;
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
 
